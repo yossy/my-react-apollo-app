dd741cdd11cee15c785570e6b9a10398eadcd158
yossy <t.yoshimura0806@gmail.com>
2021-02-14 23:50:01 +0900

    apolloの導入

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const query = gql`
+  {
+    organization(login: "apollographql") {
+      repositories(first: 5, isFork: false) {
+        nodes {
+          id
+          name
+          url
+          viewerHasStarred
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`
+
+const App = () => (
+  <Query query={query}>
+    {({ loading, data }) => {
+      if (loading) return <p>Loading...</p>
+
+      const repositories = data.organization.repositories.nodes;
+
+      return (
+        <ul>
+          {repositories.map(repo => (
+            <li key={repo.id}>
+              <a href={repo.url}>{repo.name}</a>
+              <button>{repo.stargazers.totalCount} Star</button>
+            </li>
+          ))}
+        </ul>
+      );
+    }}
+  </Query>
+)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${
+          process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+        }`
+      }
+    });
+  }
+});
+
+const query = gql`
+  {
+    organization(login: "apollographql") {
+      repositories(first: 5) {
+        nodes {
+          id
+          name
+          url
+          viewerHasStarred
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`
+
+client
+  .query({
+    query
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
